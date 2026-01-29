480201bda672981b0c7df5e4944377127e1a2609
Salinger2333 <1565664814@qq.com>
2026-01-29 19:36:11 +0800

    use provider

--- StyledButton.jsx
+++ StyledButton.jsx
@@ -2,19 +2,34 @@ import styled from "styled-components";
 
 export const StyledButton = styled.a`
   padding: 0.75em 1em;
-  background-color: ${({ primary }) => (primary ? "#07c" : "#333")};
+  background-color: ${(props) => props.theme.btnBg};
   color: white;
   display: inline-flex;
   text-decoration: none;
   border-radius: 0.25em;
+  transition:
+    background-color 0.25s ease-in-out,
+    color 0.25s ease-in-out;
 
   &:hover {
-    background-color: ${({ primary }) => (primary ? "#09c" : "#111")};
+    background-color: ${(props) => props.theme.btnHoverBg};
+    color: ${({ theme }) => theme.btnHoverColor};
   }
 `;
-
+StyledButton.defaultProps = {
+  theme: {
+    btnBg: "palevioletred", // Button background
+    btnColor: "#eee", // Button text color
+    btnHoverBg: "yellow", // Button hover background
+    btnHoverColor: "white",
+  },
+};
 export const BtnGroup = styled.button`
   display: flex;
   gap: 1rem;
-  gap: 1rem;
+`;
+
+export const GreenStyledButton = styled(StyledButton)`
+  color: green;
+  border-color: green;
 `;
